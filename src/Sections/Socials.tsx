2ee20b32b9db3f3344d3e10019d507d811ada387
Peter Hechler <peter.hechler@gmail.com>
2024-03-14 18:02:38 -0400

    update resume link

--- src/Sections/Socials.tsx
+++ src/Sections/Socials.tsx
@@ -11,7 +11,7 @@ const socialsData: SocialButtonProps[] = [
     {
         icon: <DescriptionIcon />,
         label: "Resume",
-        href: "https://drive.google.com/file/d/1ihZss0njJMqdNAG0Na2YrgF41BrpTPfo/view?usp=sharing"
+        href: "https://drive.google.com/file/d/1ETbjKq9oAUNDGMdvKYlg0_AMXdCH7Wrp/view?usp=sharing"
     },
     {
         icon: <LinkedInIcon />,
